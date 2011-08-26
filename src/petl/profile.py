54579d3ef426df04e9d683efc7d6fe01478e4c33
Alistair Miles <alimanfoo@googlemail.com>
2011-08-26 10:34:03 +0100

    some more work on profiling

--- src/petl/profile.py
+++ src/petl/profile.py
@@ -4,8 +4,13 @@ TODO doc me
 """
 
 
+import logging
 from itertools import islice
-from collections import defaultdict
+from collections import defaultdict, Counter
+
+
+logger = logging.getLogger('petl')
+d, i, w, e = logger.debug, logger.info, logger.warn, logger.error # abbreviations
 
 
 class Profiler(object):
@@ -28,10 +33,10 @@ class Profiler(object):
 
     def profile(self):
         
+        d('setup the report')
         report = dict()
         report['table'] = dict()
         report['field'] = dict()
-
         it = iter(self._table)
         fields = it.next()
         report['table']['default'] = dict()
@@ -39,10 +44,11 @@ class Profiler(object):
         for f in fields:
             report['field'][f] = dict()
 
-        table_analyses = [cls() for cls, field in self._analyses if isinstance(cls, TableAnalysis)]
+        d('setup analyses')
+        table_analyses = [cls() for cls, field in self._analyses if issubclass(cls, TableAnalysis)]
         field_analyses = defaultdict(list)
         for cls, field in self._analyses:
-            if isinstance(cls, FieldAnalysis):
+            if issubclass(cls, FieldAnalysis):
                 if field is not None:
                     # add analysis on a single field
                     field_analyses[field].append(cls())
@@ -51,9 +57,11 @@ class Profiler(object):
                     for field in fields:
                         field_analyses[field].append(cls())
         
+        d('deal with sampling')
         if self._max_sample_size:
             it = islice(it, 0, self._max_sample_size-1)
 
+        d('profile the data')
         sample_size = 0
         for row in it:
             sample_size += 1
@@ -69,12 +77,20 @@ class Profiler(object):
                     for analysis in field_analyses[field]:
                         analysis.accept(value)
         
+        d('build the report')
         report['table']['default']['sample_size'] = sample_size
         for analysis in table_analyses:
-            analysis.report(report['table'])
+            key, data = analysis.report()
+            d(key)
+            d(data)
+            report['table'][key] = data
         for field in fields:
             for analysis in field_analyses[field]:
-                analysis.report(report['field'][field])
+                key, data = analysis.report()
+                d(field)
+                d(key)
+                d(data)
+                report['field'][field][key] = data
 
         return report
 
@@ -93,36 +109,99 @@ class FieldAnalysis(Analysis):
 
 class RowLengths(TableAnalysis):
 
+    def __init__(self):
+        self._row_lengths_sum = 0
+        self._row_count = 0
+        self._min_row_length = None
+        self._max_row_length = None
+        
     def accept(self, row):
-        pass
+        n = len(row)
+        self._row_count += 1
+        self._row_lengths_sum += n
+        if self._min_row_length is None or n < self._min_row_length:
+            self._min_row_length = n
+        if self._max_row_length is None or n > self._max_row_length:
+            self._max_row_length = n
 
-    def report(self, report):
-        pass
+    def report(self):
+        d('build a report of row lengths data')
+        data = {
+                'row_count': self._row_count,
+                'row_lengths_sum': self._row_lengths_sum,
+                'min_row_length': self._min_row_length,
+                'max_row_length': self._max_row_length,
+                'mean_row_length': self._row_lengths_sum / self._row_count
+                }
+        return ('row_lengths', data)
     
 
 class DistinctValues(FieldAnalysis):
+    
+    def __init__(self):
+        self._counter = Counter()
 
     def accept(self, value):
-        pass
+        self._counter[value] += 1
 
-    def report(self, report):
-        pass
+    def report(self):
+        d('build a report of distinct values')
+        return ('distinct_values', self._counter)
 
 
 class BasicStatistics(FieldAnalysis):
 
-    def accept(self, value):
-        pass
+    def __init__(self):
+        self._count = 0
+        self._errors = 0
+        self._sum = 0
+        self._max = None
+        self._min = None
 
-    def report(self, report):
-        pass
+    def accept(self, value):
+        try:
+            value = float(value)
+        except ValueError:
+            self._errors += 1
+        else:
+            self._count += 1
+            self._sum += value
+            if self._min is None or value < self._min:
+                self._min = value
+            if self._max is None or value > self._max:
+                self._max = value
+
+    def report(self):
+        d('build a report of basic statistics')
+        data = {
+                'min': self._min,
+                'max': self._max,
+                'sum': self._sum,
+                'count': self._count,
+                'errors': self._errors
+                }
+        if self._count > 0:
+            data['mean'] = self._sum / self._count
+
+        return ('basic_statistics', data)
     
 
 class Types(FieldAnalysis):
+    
+    def __init__(self):
+        self._actual_types = Counter()
+        self._applicable_types = Counter()
+        self._inferred_type = None
 
     def accept(self, value):
+        # TODO
         pass
 
-    def report(self, report):
-        pass
+    def report(self):
+        data = {
+                'actual_types': self._actual_types,
+                'applicable_types': self._applicable_types,
+                'inferred_type': self._inferred_type
+                }
+        return ('types', data)
     
--- src/petl/test/test_profile.py
+++ src/petl/test/test_profile.py
@@ -4,7 +4,23 @@ TODO doc me
 """
 
 
+import logging
+import sys
+
+
 from petl.profile import *
+
+
+logger = logging.getLogger('petl')
+d, i, w, e = logger.debug, logger.info, logger.warn, logger.error # abbreviations
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(stream=sys.stderr)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(funcName)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+d('begin tests')
 
 
 def test_profile():
@@ -22,35 +38,34 @@ def test_profile():
 
     profiler = Profiler(table)
 
-    # profile the table with default analyses - list field names and
-    # report the sample size used for profiling
+    d('profile the table with default analyses')
     report = profiler.profile()
     assert report['table']['default']['fields'] == ('foo', 'bar')
     assert report['table']['default']['sample_size'] == 5
 
-    # add row lengths analysis
+    d('add row lengths analysis')
     profiler.add(RowLengths)
     report = profiler.profile()
     assert report['table']['row_lengths']['max_row_length'] == 3
     assert report['table']['row_lengths']['min_row_length'] == 1
     assert report['table']['row_lengths']['mean_row_length'] == 2
 
-    # add distinct values analysis on field 'foo'
+    d('add distinct values analysis on field "foo"')
     profiler.add(DistinctValues, field='foo')
     report = profiler.profile()
     assert report['field']['foo']['distinct_values'] == {'A': 1, 'B': 2, 'D': 1, 'E': 1}
 
-    # add basic statistics analysis on field 'bar'
-    profiler.add(BasicStatistics, field='foo')
+    d('add basic statistics analysis on field "bar"')
+    profiler.add(BasicStatistics, field='bar')
     report = profiler.profile()
-    assert report['field']['bar']['basic_statistics']['min'] == 1
-    assert report['field']['bar']['basic_statistics']['max'] == 3
-    assert report['field']['bar']['basic_statistics']['mean'] == 2
-    assert report['field']['bar']['basic_statistics']['sum'] == 6
+    assert report['field']['bar']['basic_statistics']['min'] == 1.0
+    assert report['field']['bar']['basic_statistics']['max'] == 3.0
+    assert report['field']['bar']['basic_statistics']['mean'] == 2.0
+    assert report['field']['bar']['basic_statistics']['sum'] == 6.0
     assert report['field']['bar']['basic_statistics']['count'] == 3
-    assert report['field']['bar']['basic_statistics']['errors'] == 2
+    assert report['field']['bar']['basic_statistics']['errors'] == 1
 
-    # add types analysis on all fields
+    d('add types analysis on all fields')
     profiler.add(Types) 
     report = profiler.profile()
     assert report['field']['foo']['types']['actual_types'] == {'string': 5}
@@ -59,3 +74,4 @@ def test_profile():
     assert report['field']['bar']['types']['actual_types'] == {'int': 2, 'string': 2}
     assert report['field']['bar']['types']['applicable_types'] == {'int': 3, 'float': 3, 'string': 5}     
     assert report['field']['bar']['types']['inferred_type'] == 'int'    
+
