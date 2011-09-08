586ea4bc074048896fabdf490376a15dcb34e816
Alistair Miles <alimanfoo@googlemail.com>
2011-09-08 11:16:56 +0100

    released 0.1-alpha

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@ from distutils.core import setup
 
 setup(
     name='petl',
-    version='0.1-SNAPSHOT',
+    version='0.1-alpha',
     author='Alistair Miles',
     author_email='alimanfoo@googlemail.com',
     package_dir={'': 'src'},
     packages=['petl'],
-    url='http://pypi.python.org/pypi/petl/',
-    license='LICENSE.txt',
+    url='https://github.com/alimanfoo/petl',
+    license='MIT License',
     description='A tentative Python module for extracting, transforming and loading tables of data.',
     long_description=open('README.txt').read(),
+    classifiers=['Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Libraries :: Python Modules'
+                 ]
 )
