1188e5882c8db8aab104dfe47ec89d80a9d688ff
Alistair Miles <alimanfoo@googlemail.com>
2011-10-20 22:16:58 +0100

    some work on util

--- src/petl/__init__.py
+++ src/petl/__init__.py
@@ -1,34 +1,8 @@
-def fields(table):
-    """TODO doc me"""
-    
-def data(table):
-    """TODO doc me"""
-    
-def records(table):
-    """TODO doc me"""
-    
-def count(table):
-    """TODO doc me"""
-    
-def look(table):
-    """TODO doc me"""
-    
-def see(table):
-    """TODO doc me"""
-    
-def values(table):
-    """TODO doc me"""
-    
-def types(table):
-    """TODO doc me"""
-    
-def parsetypes(table):
-    """TODO doc me"""
-    
-def stats(table):
-    """TODO doc me"""
-    
-def rowlengths(table):
-    """TODO doc me"""
-    
-    
+"""
+TODO doc me
+
+"""
+
+__all__ = ['util']
+
+from petl.util import *    
