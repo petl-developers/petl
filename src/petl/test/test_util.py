787b76f394ee30b6413350f47b5c6c1154451e1d
Alistair Miles <alimanfoo@googlemail.com>
2011-10-21 17:31:41 +0100

    some more work on util

--- src/petl/test/test_util.py
+++ src/petl/test/test_util.py
@@ -3,43 +3,133 @@ TODO doc me
 
 """
 
-from petl import fields, data, records
+from petl import fields, data, records, count, look, see, values, valueset
 
 
-def itercompare(it1, it2):
-    for x, y in zip(it1, it2):
-        assert x == y, (x, y)
+def assertequal(expect, actual):
+    assert expect == actual, (expect, actual)
+
+
+def iassertequal(expect, actual):
+    for e, a in zip(expect, actual):
+        assert e == a, (e, a)
 
 
 def test_fields():
     """Test the fields function."""
     
     table = [['foo', 'bar'], ['a', 1], ['b', 2]]
-    expectation = ['foo', 'bar']
-    assert expectation == fields(table)
+    expect = ['foo', 'bar']
+    actual = fields(table)
+    assertequal(expect, actual)
     
 
 def test_data():
     """Test the data function."""
 
     table = [['foo', 'bar'], ['a', 1], ['b', 2]]
-    expectation = [['a', 1], ['b', 2]]
-    itercompare(expectation, data(table))
+    expect = [['a', 1], ['b', 2]]
+    actual = data(table)
+    iassertequal(expect, actual)
 
 
 def test_records():
     """Test the records function."""
 
     table = [['foo', 'bar'], ['a', 1], ['b', 2]]
-    expectation = [{'foo': 'a', 'bar': 1}, {'foo': 'b', 'bar': 2}]
-    itercompare(expectation, records(table))
+    expect = [{'foo': 'a', 'bar': 1}, {'foo': 'b', 'bar': 2}]
+    actual = records(table)
+    iassertequal(expect, actual)
     
         
 def test_records_shortrows():
     """Test the records function on a table with short rows."""
 
     table = [['foo', 'bar'], ['a', 1], ['b']]
-    expectation = [{'foo': 'a', 'bar': 1}, {'foo': 'b', 'bar': None}]
-    itercompare(expectation, records(table))
+    expect = [{'foo': 'a', 'bar': 1}, {'foo': 'b', 'bar': None}]
+    actual = records(table)
+    iassertequal(expect, actual)
     
     
+def test_count():
+    """Test the count function."""
+    
+    table = [['foo', 'bar'], ['a', 1], ['b']]
+    expect = 2
+    actual = count(table)
+    assertequal(expect, actual)
+    
+    
+def test_look():
+    """Test the look function."""
+    
+    table = [['foo', 'bar'], ['a', 1], ['b', 2]]
+    expect = u"""+-------+-------+
+| 'foo' | 'bar' |
++=======+=======+
+| 'a'   | 1     |
++-------+-------+
+| 'b'   | 2     |
++-------+-------+
+"""
+    actual = repr(look(table))
+    assertequal(expect, actual)
+
+        
+def test_look_irregular_rows():
+    """Test the look function with a table where row lengths are irregular."""
+    
+    table = [['foo', 'bar'], ['a'], ['b', 2, True]]
+    expect = u"""+-------+-------+------+
+| 'foo' | 'bar' |      |
++=======+=======+======+
+| 'a'   |       |      |
++-------+-------+------+
+| 'b'   | 2     | True |
++-------+-------+------+
+"""
+    actual = repr(look(table))
+    assertequal(expect, actual)
+
+    
+def test_see():
+    """Test the see function."""
+    
+    table = [['foo', 'bar'], ['a', 1], ['b', 2]]
+    expect = u"""'foo': 'a', 'b'
+'bar': 1, 2
+"""
+    actual = repr(see(table))
+    assertequal(expect, actual)
+
+        
+def test_values():
+    """Test the values function."""
+    
+    table = [['foo', 'bar'], ['a', 1], ['b', 2], ['b', 7]]
+    expect = (('value', 'count'), ('b', 2), ('a', 1))
+    actual = values(table, 'foo')
+    iassertequal(expect, actual) 
+
+
+def test_values_shortrows():
+    """Test the values function with short rows."""
+    
+    table = [['foo', 'bar'], ['a', True], ['b'], ['b', True], ['c', False]]
+    expect = (('value', 'count'), (True, 2), (False, 1))
+    actual = values(table, 'bar')
+    iassertequal(expect, actual) 
+
+    
+def test_valueset():
+    """Test the valueset function."""
+
+    table = [['foo', 'bar'], ['a', True], ['b'], ['b', True], ['c', False]]
+    expect = {'a', 'b', 'c'}
+    actual = valueset(table, 'foo')
+    assertequal(expect, actual)
+    expect = {True, False}
+    actual = valueset(table, 'bar')
+    assertequal(expect, actual)
+
+
--- src/petl/util.py
+++ src/petl/util.py
@@ -5,17 +5,17 @@ TODO doc me
 
 
 from itertools import islice
+from collections import defaultdict, Counter
 
 
 __all__ = ['fields', 'data', 'records', 'count', 'look', 'see', 'values', 
-           'types', 'parsetypes', 'stats', 'rowlengths']
+           'valueset', 'types', 'parsetypes', 'stats', 'rowlengths']
 
 
 def fields(table):
     """
     Return the header row for the given table. E.g.::
     
-        >>> from petl import fields
         >>> table = [['foo', 'bar'], ['a', 1], ['b', 2]]
         >>> fields(table)
         ['foo', 'bar']
@@ -23,14 +23,20 @@ def fields(table):
     """
     
     it = iter(table)
-    return it.next()
+    flds = None
+    try:
+        flds = it.next()
+    except:
+        raise
+    finally:
+        close(it)
+    return flds
 
     
 def data(table):
     """
     Return an iterator over the data rows for the given table. E.g.::
     
-        >>> from petl import data
         >>> table = [['foo', 'bar'], ['a', 1], ['b', 2]]
         >>> it = data(table)
         >>> it.next()
@@ -45,8 +51,8 @@ def data(table):
     
 def records(table, missing=None):
     """
-    Return an iterator over the data in the table as a sequence of dictionaries.
-    E.g.::
+    Return an iterator over the data in the table, yielding each row as a 
+    dictionary of values indexed by field. E.g.::
     
         >>> from petl import records
         >>> table = [['foo', 'bar'], ['a', 1], ['b', 2]]
@@ -59,15 +65,15 @@ def records(table, missing=None):
     """
     
     it = iter(table)
-    fields = it.next()
+    flds = it.next()
     for row in it:
         try:
             # list comprehension should be faster
-            items = [(fields[i], row[i]) for i in range(len(fields))]
+            items = [(flds[i], row[i]) for i in range(len(flds))]
         except IndexError:
             # short row, fall back to slower for loop
             items = list()
-            for i, f in enumerate(fields):
+            for i, f in enumerate(flds):
                 try:
                     v = row[i]
                 except IndexError:
@@ -77,21 +83,279 @@ def records(table, missing=None):
     
     
 def count(table):
-    """TODO doc me"""
+    """
+    Count the number of rows in a table. E.g.::
+    
+        >>> table = [['foo', 'bar'], ['a', 1], ['b', 2]]
+        >>> count(table)
+        2
+    """
+    
+    n = 0
+    for row in data(table):
+        n += 1
+    return n
     
     
-def look(table):
-    """TODO doc me"""
+def look(table, start=0, stop=10, step=1):
+    """
+    Format a portion of the table as text for inspection in an interactive
+    session. E.g.::
+    
+        >>> table = [['foo', 'bar'], ['a', 1], ['b', 2]]
+        >>> look(table)
+        +-------+-------+
+        | 'foo' | 'bar' |
+        +=======+=======+
+        | 'a'   | 1     |
+        +-------+-------+
+        | 'b'   | 2     |
+        +-------+-------+
+
+    Any irregularities in the length of header and/or data rows will appear as
+    blank cells, e.g.::
+    
+        >>> table = [['foo', 'bar'], ['a'], ['b', 2, True]]
+        >>> look(table)
+        +-------+-------+------+
+        | 'foo' | 'bar' |      |
+        +=======+=======+======+
+        | 'a'   |       |      |
+        +-------+-------+------+
+        | 'b'   | 2     | True |
+        +-------+-------+------+
+        
+    """
+    
+    return Look(table, start, stop, step)
     
     
-def see(table):
-    """TODO doc me"""
+class Look(object):
+    
+    def __init__(self, table, start=0, stop=10, step=1):
+        self.table = table
+        self.start = start
+        self.stop = stop
+        self.step = step
+        
+    def __repr__(self):
+        it = iter(self.table)
+        try:
+            
+            # fields representation
+            flds = it.next()
+            fldsrepr = [repr(f) for f in flds]
+            
+            # rows representations
+            rows = list(islice(it, self.start, self.stop, self.step))
+            rowsrepr = [[repr(v) for v in row] for row in rows]
+            
+            # find maximum row length - may be uneven
+            rowlens = [len(flds)]
+            rowlens.extend([len(row) for row in rows])
+            maxrowlen = max(rowlens)
+            
+            # pad short fields and rows
+            if len(flds) < maxrowlen:
+                fldsrepr.extend([u''] * (maxrowlen - len(flds)))
+            for valsrepr in rowsrepr:
+                if len(valsrepr) < maxrowlen:
+                    valsrepr.extend([u''] * (maxrowlen - len(valsrepr)))
+            
+            # find longest representations so we know how wide to make cells
+            colwidths = [0] * maxrowlen # initialise to 0
+            for i, fr in enumerate(fldsrepr):
+                colwidths[i] = len(fr)
+            for valsrepr in rowsrepr:
+                for i, vr in enumerate(valsrepr):
+                    if len(vr) > colwidths[i]:
+                        colwidths[i] = len(vr)
+                        
+            # construct a line separator
+            sep = u'+'
+            for w in colwidths:
+                sep += u'-' * (w + 2)
+                sep += u'+'
+            sep += u'\n'
+            
+            # construct a header separator
+            hedsep = u'+'
+            for w in colwidths:
+                hedsep += u'=' * (w + 2)
+                hedsep += u'+'
+            hedsep += u'\n'
+            
+            # construct a line for the header row
+            fldsline = u'|'
+            for i, w in enumerate(colwidths):
+                f = fldsrepr[i]
+                fldsline += u' ' + f
+                fldsline += u' ' * (w - len(f)) # padding
+                fldsline += u' |'
+            fldsline += u'\n'
+            
+            # construct a line for each data row
+            rowlines = list()
+            for valsrepr in rowsrepr:
+                rowline = u'|'
+                for i, w in enumerate(colwidths):
+                    v = valsrepr[i]
+                    rowline += u' ' + v
+                    rowline += u' ' * (w - len(v)) # padding
+                    rowline += u' |'
+                rowline += u'\n'
+                rowlines.append(rowline)
+                
+            # put it all together
+            output = sep + fldsline + hedsep
+            for line in rowlines:
+                output += line + sep
+            
+            return output
+        except:
+            raise
+        finally:
+            close(it)
     
     
-def values(table):
-    """TODO doc me"""
+    def __str__(self):
+        return repr(self)
+        
+        
+def see(table, start=0, stop=5, step=1):
+    """
+    Format a portion of a table as text in a column-oriented layout for 
+    inspection in an interactive session. E.g.::
+    
+        >>> table = [['foo', 'bar'], ['a', 1], ['b', 2]]
+        >>> see(table)
+        'foo': 'a', 'b'
+        'bar': 1, 2
+
+    Useful for tables with a larger number of fields.
+    
+    """
+
+    return See(table, start, stop, step)
+
+
+class See(object):
+    
+    def __init__(self, table, start=0, stop=5, step=1):
+        self.table = table
+        self.start = start
+        self.stop = stop
+        self.step = step
+        
+    def __repr__(self):    
+        it = iter(self.table)
+        try:
+            flds = it.next()
+            cols = defaultdict(list)
+            for row in islice(it, self.start, self.stop, self.step):
+                for i, f in enumerate(flds):
+                    try:
+                        cols[f].append(repr(row[i]))
+                    except IndexError:
+                        cols[f].append('')
+            close(it)
+            output = u''
+            for f in flds:
+                output += u'%r: %s\n' % (f, u', '.join(cols[f]))
+            return output
+        
+        except:
+            raise
+        finally:
+            close(it)
     
     
+def values(table, field, start=0, stop=None, step=1):    
+    """
+    Find distinct values for the given field and count the number of 
+    occurrences. Returns a table mapping values to counts. E.g.::
+
+        >>> table = [['foo', 'bar'], ['a', True], ['b'], ['b', True], ['c', False]]
+        >>> values(table, 'foo')
+        [('value', 'count'), ('b', 2), ('a', 1), ('c', 1)]
+
+    Can be combined with `look`, e.g.::
+    
+        >>> table = [['foo', 'bar'], ['a', True], ['b'], ['b', True], ['c', False]]
+        >>> look(values(table, 'foo'))
+        +---------+---------+
+        | 'value' | 'count' |
+        +=========+=========+
+        | 'b'     | 2       |
+        +---------+---------+
+        | 'a'     | 1       |
+        +---------+---------+
+        | 'c'     | 1       |
+        +---------+---------+
+        
+        >>> look(values(table, 'bar'))
+        +---------+---------+
+        | 'value' | 'count' |
+        +=========+=========+
+        | True    | 2       |
+        +---------+---------+
+        | False   | 1       |
+        +---------+---------+
+            
+    """
+    
+    it = iter(table)
+    try:
+        flds = it.next()
+        assert field in flds, 'field not found: %s' % field
+        field_index = flds.index(field)
+        it = islice(it, start, stop, step)
+        counter = Counter()
+        for row in it:
+            try:
+                counter[row[field_index]] += 1
+            except IndexError:
+                pass # short row
+        output = [('value', 'count')]
+        output.extend(counter.most_common())
+        return output
+    except:
+        raise
+    finally:
+        close(it)
+        
+        
+def valueset(table, field, start=0, stop=None, step=1):
+    """
+    Find distinct values for the given field. Returns a set. E.g.::
+
+        >>> table = [['foo', 'bar'], ['a', True], ['b'], ['b', True], ['c', False]]
+        >>> valueset(table, 'foo')
+        set(['a', 'c', 'b'])
+        >>> valueset(table, 'bar')
+        set([False, True])
+        
+    """
+
+    it = iter(table)
+    try:
+        flds = it.next()
+        assert field in flds, 'field not found: %s' % field
+        field_index = flds.index(field)
+        it = islice(it, start, stop, step)
+        vals = set()
+        for row in it:
+            try:
+                vals.add(row[field_index])
+            except IndexError:
+                pass # short row
+        return vals
+    except:
+        raise
+    finally:
+        close(it)
+    
+        
 def types(table):
     """TODO doc me"""
     
@@ -107,3 +371,15 @@ def stats(table):
 def rowlengths(table):
     """TODO doc me"""
     
+    
+def close(o):
+    """
+    If the object has a 'close' method, call it. 
+    
+    """
+
+    if hasattr(o, 'close') and callable(getattr(o, 'close')):
+        o.close()
+        
+        
+
