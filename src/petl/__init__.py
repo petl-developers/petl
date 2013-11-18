1b9bc9faed57ab1dc4e6fe831cecaf6a22b26781
Alistair Miles <alimanfoo@googlemail.com>
2013-11-18 22:21:47 +0000

    v0.18.1

--- src/petl/__init__.py
+++ src/petl/__init__.py
@@ -7,7 +7,8 @@ The `petl` module.
 #0.16.2
 #0.17
 #0.17.1
-VERSION = '0.19-SNAPSHOT'
+#0.18
+__version__ = VERSION = '0.18.1'
 
 
 from petl.util import header, fieldnames, data, records, rowcount, look, see, \
