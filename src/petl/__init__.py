daa7c8a821bfae0897ab73fceb9a8f9ee7d290cb
Alistair Miles <alimanfoo@googlemail.com>
2014-01-13 10:56:39 +0000

    increment version

--- src/petl/__init__.py
+++ src/petl/__init__.py
@@ -10,7 +10,8 @@ The `petl` module.
 #0.18
 #0.18.1
 #0.19
-__version__ = VERSION = '0.20'
+#0.20
+__version__ = VERSION = '0.21-SNAPSHOT'
 
 
 from petl.util import header, fieldnames, data, records, rowcount, look, see, \
