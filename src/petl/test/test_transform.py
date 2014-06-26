befc477056637be76f0a9ff260a5093a032c88d3
Alistair Miles <alimanfoo@googlemail.com>
2014-06-26 12:07:01 +0100

    #267 factor out petl.test.transform.test_dedup

--- src/petl/test/test_transform.py
+++ src/petl/test/test_transform.py
@@ -11,8 +11,8 @@ import operator
 from petl.util import OrderedDict
 from petl.testutils import ieq
 from petl import rename, fieldnames, cut, cat, addfield, \
-    rowslice, head, tail, sort, melt, recast, duplicates, \
-    conflicts, mergeduplicates, select, complement, diff, capture, \
+    rowslice, head, tail, sort, melt, recast, \
+    mergeduplicates, select, complement, diff, capture, \
     split, expr, fieldmap, facet, rowreduce, aggregate, \
     rowmap, recordmap, rowmapmany, setheader, pushheader, \
     skip, extendheader, unpack, rangeaggregate, rangecounts, \
@@ -20,7 +20,7 @@ from petl import rename, fieldnames, cut, cat, addfield, \
     rowlenselect, strjoin, transpose, intersection, pivot, \
     recorddiff, recordcomplement, cutout, skipcomments, \
     hashcomplement, hashintersection, \
-    flatten, unflatten, mergesort, annex, unpackdict, unique, \
+    flatten, unflatten, mergesort, annex, unpackdict, \
     selectin, fold, addrownumbers, selectcontains, search, \
     addcolumn, filldown, fillright, \
     fillleft, multirangeaggregate, unjoin, coalesce, nrows, \
@@ -897,168 +897,6 @@ def test_recast_date():
 
     result = recast(table, key='id', variablefield='variable', valuefield='value')
     ieq(expectation, result)
-
-
-
-def test_duplicates():
-    
-    table = (('foo', 'bar', 'baz'),
-             ('A', 1, 2),
-             ('B', '2', '3.4'),
-             ('D', 'xyz', 9.0),
-             ('B', u'3', u'7.8', True),
-             ('B', '2', 42),
-             ('E', None),
-             ('D', 4, 12.3))
-
-    result = duplicates(table, 'foo')
-    expectation = (('foo', 'bar', 'baz'),
-                   ('B', '2', '3.4'),
-                   ('B', u'3', u'7.8', True),
-                   ('B', '2', 42),
-                   ('D', 'xyz', 9.0),
-                   ('D', 4, 12.3))
-    ieq(expectation, result)
-    
-    # test with compound key
-    result = duplicates(table, key=('foo', 'bar'))
-    expectation = (('foo', 'bar', 'baz'),
-                   ('B', '2', '3.4'),
-                   ('B', '2', 42))
-    ieq(expectation, result)
-    
-
-def test_duplicates_empty():
-    table = (('foo', 'bar'),)
-    expect = (('foo', 'bar'),)
-    actual = duplicates(table, key='foo')
-    ieq(expect, actual)
-
-
-def test_duplicates_wholerow():
-    
-    table = (('foo', 'bar', 'baz'),
-             ('A', 1, 2),
-             ('B', '2', '3.4'),
-             ('B', '2', '3.4'),
-             ('D', 4, 12.3))
-
-    result = duplicates(table)
-    expectation = (('foo', 'bar', 'baz'),
-             ('B', '2', '3.4'),
-             ('B', '2', '3.4'))
-    ieq(expectation, result)
-    
-
-def test_unique():
-    
-    table = (('foo', 'bar', 'baz'),
-             ('A', 1, 2),
-             ('B', '2', '3.4'),
-             ('D', 'xyz', 9.0),
-             ('B', u'3', u'7.8', True),
-             ('B', '2', 42),
-             ('E', None),
-             ('D', 4, 12.3),
-             ('F', 7, 2.3))
-
-    result = unique(table, 'foo')
-    expectation = (('foo', 'bar', 'baz'),
-                   ('A', 1, 2),
-                   ('E', None),
-                   ('F', 7, 2.3))
-    ieq(expectation, result)
-    ieq(expectation, result)
-    
-    # test with compound key
-    result = unique(table, key=('foo', 'bar'))
-    expectation = (('foo', 'bar', 'baz'),
-                   ('A', 1, 2),
-                   ('B', u'3', u'7.8', True),
-                   ('D', 4, 12.3),
-                   ('D', 'xyz', 9.0),
-                   ('E', None),
-                   ('F', 7, 2.3))
-    ieq(expectation, result)
-    ieq(expectation, result)
-    
-
-def test_unique_empty():
-    table = (('foo', 'bar'),)
-    expect = (('foo', 'bar'),)
-    actual = unique(table, key='foo')
-    ieq(expect, actual)
-
-
-def test_unique_wholerow():
-    
-    table = (('foo', 'bar', 'baz'),
-             ('A', 1, 2),
-             ('B', '2', '3.4'),
-             ('B', '2', '3.4'),
-             ('D', 4, 12.3))
-
-    result = unique(table)
-    expectation = (('foo', 'bar', 'baz'),
-             ('A', 1, 2),
-             ('D', 4, 12.3))
-    ieq(expectation, result)
-    
-
-def test_conflicts():
-    
-    table = (('foo', 'bar', 'baz'),
-             ('A', 1, 2),
-             ('B', '2', None),
-             ('D', 'xyz', 9.4),
-             ('B', None, u'7.8', True),
-             ('E', None),
-             ('D', 'xyz', 12.3),
-             ('A', 2, None))
-
-    result = conflicts(table, 'foo', missing=None)
-    expectation = (('foo', 'bar', 'baz'),
-                   ('A', 1, 2),
-                   ('A', 2, None),
-                   ('D', 'xyz', 9.4),
-                   ('D', 'xyz', 12.3))
-    ieq(expectation, result)
-    ieq(expectation, result)
-    
-    result = conflicts(table, 'foo', missing=None, exclude='baz')
-    expectation = (('foo', 'bar', 'baz'),
-                   ('A', 1, 2),
-                   ('A', 2, None))
-    ieq(expectation, result)
-    ieq(expectation, result)
-    
-    result = conflicts(table, 'foo', missing=None, exclude=('bar', 'baz'))
-    expectation = (('foo', 'bar', 'baz'),)
-    ieq(expectation, result)
-    ieq(expectation, result)
-
-    result = conflicts(table, 'foo', missing=None, include='bar')
-    expectation = (('foo', 'bar', 'baz'),
-                   ('A', 1, 2),
-                   ('A', 2, None))
-    ieq(expectation, result)
-    ieq(expectation, result)
-    
-    result = conflicts(table, 'foo', missing=None, include=('bar', 'baz'))
-    expectation = (('foo', 'bar', 'baz'),
-                   ('A', 1, 2),
-                   ('A', 2, None),
-                   ('D', 'xyz', 9.4),
-                   ('D', 'xyz', 12.3))
-    ieq(expectation, result)
-    ieq(expectation, result)
-
-    
-def test_conflicts_empty():
-    table = (('foo', 'bar'),)
-    expect = (('foo', 'bar'),)
-    actual = conflicts(table, key='foo')
-    ieq(expect, actual)
 
 
 def test_mergeduplicates():
