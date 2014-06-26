47d397452f698d6644cc7c9775ef88bf056a501d
Alistair Miles <alimanfoo@googlemail.com>
2014-06-26 12:08:48 +0100

    #267 factor out petl.test.transform.test_fills

--- src/petl/test/test_transform.py
+++ src/petl/test/test_transform.py
@@ -22,8 +22,8 @@ from petl import rename, fieldnames, cut, cat, addfield, \
     hashcomplement, hashintersection, \
     flatten, unflatten, mergesort, annex, unpackdict, \
     selectin, fold, addrownumbers, selectcontains, search, \
-    addcolumn, filldown, fillright, \
-    fillleft, multirangeaggregate, unjoin, coalesce, nrows, \
+    addcolumn, \
+    multirangeaggregate, unjoin, coalesce, nrows, \
     empty, selectusingcontext, addfieldusingcontext, \
     prefixheader, suffixheader, movefield
 from petl.transform.misc import TransformError
@@ -3214,102 +3214,6 @@ def test_empty_addcolumn():
     ieq(expect, table3)
     ieq(expect, table3)
 
-
-def test_filldown():
-    
-    table = (('foo', 'bar', 'baz'),
-             (1, 'a', None),
-             (1, None, .23),
-             (1, 'b', None),
-             (2, None, None),
-             (2, None, .56),
-             (2, 'c', None),
-             (None, 'c', .72))
-    
-    actual = filldown(table)
-    expect = (('foo', 'bar', 'baz'),
-              (1, 'a', None),
-              (1, 'a', .23),
-              (1, 'b', .23),
-              (2, 'b', .23),
-              (2, 'b', .56),
-              (2, 'c', .56),
-              (2, 'c', .72))
-    ieq(expect, actual)
-    ieq(expect, actual)
-
-    actual = filldown(table, 'bar')
-    expect = (('foo', 'bar', 'baz'),
-              (1, 'a', None),
-              (1, 'a', .23),
-              (1, 'b', None),
-              (2, 'b', None),
-              (2, 'b', .56),
-              (2, 'c', None),
-              (None, 'c', .72))
-    ieq(expect, actual)
-    ieq(expect, actual)
-
-    actual = filldown(table, 'foo', 'bar')
-    expect = (('foo', 'bar', 'baz'),
-              (1, 'a', None),
-              (1, 'a', .23),
-              (1, 'b', None),
-              (2, 'b', None),
-              (2, 'b', .56),
-              (2, 'c', None),
-              (2, 'c', .72))
-    ieq(expect, actual)
-    ieq(expect, actual)
-
-
-def test_fillright():
-    
-    table = (('foo', 'bar', 'baz'),
-             (1, 'a', None),
-             (1, None, .23),
-             (1, 'b', None),
-             (2, None, None),
-             (2, None, .56),
-             (2, 'c', None),
-             (None, 'c', .72))
-    
-    actual = fillright(table)
-    expect = (('foo', 'bar', 'baz'),
-              (1, 'a', 'a'),
-              (1, 1, .23),
-              (1, 'b', 'b'),
-              (2, 2, 2),
-              (2, 2, .56),
-              (2, 'c', 'c'),
-              (None, 'c', .72))
-    ieq(expect, actual)
-    ieq(expect, actual)
-
-
-def test_fillleft():
-    
-    table = (('foo', 'bar', 'baz'),
-             (1, 'a', None),
-             (1, None, .23),
-             (1, 'b', None),
-             (2, None, None),
-             (None, None, .56),
-             (2, 'c', None),
-             (None, 'c', .72))
-    
-    actual = fillleft(table)
-    expect = (('foo', 'bar', 'baz'),
-              (1, 'a', None),
-              (1, .23, .23),
-              (1, 'b', None),
-              (2, None, None),
-              (.56, .56, .56),
-              (2, 'c', None),
-              ('c', 'c', .72))
-    ieq(expect, actual)
-    ieq(expect, actual) 
-    
 
 def test_multirangeaggregate():
 
