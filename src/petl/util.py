8641d30051ab6e260dadce3edcf7f153bae057a6
andrewakim <kimandr2@me.com>
2014-10-14 11:47:11 -0700

    #142 - Running mean and sd calculated for batchtime and batchrate.  This can also be used to resolve #149.

--- src/petl/util.py
+++ src/petl/util.py
@@ -2886,9 +2886,14 @@ class ProgressView(RowContainer):
     def __iter__(self):
         start = time.time()
         batchstart = start
-        batchstats = {}
         ss = 0
         batches = ('time','rate')
+        batchstats = {}
+        for param in batches:
+            batchstats['%s_mean' % param] = 0
+            batchstats['%s_mean_prv' % param] = 0
+            batchstats['%s_variance' % param] = 0
+            batchstats['%s_sd' % param] = 0
         for n, r in enumerate(self.wrapped):
             if n % self.batchsize == 0 and n > 0:
                 batchend = time.time()
@@ -2902,20 +2907,11 @@ class ProgressView(RowContainer):
                     batchrate = int(self.batchsize / batchtime)
                 except ZeroDivisionError:
                     batchrate = 0
-                if ss == 0:
-                    batchstats['rate_mean_prv'] = 0
-                    batchstats['time_mean_prv'] = 0
-                    batchstats['rate_variance'] = 0
-                    batchstats['time_variance'] = 0
-
                 ss += 1
-                batchstats['time'] = batchtime
-                batchstats['rate'] = batchrate
-
                 for param in batches:
-
-                    batchstats['%s_mean' % param] = (((ss - 1)*batchstats['%s_mean_prv' % param]) + batchstats['%s' % param])/ss
-                    batchstats['%s_variance' % param] = (((ss - 1)*batchstats['%s_variance' % param]) + (batchstats['%s' % param] - batchstats['%s_mean_prv' % param])*(batchstats['%s' % param] - batchstats['%s_mean' % param]))/ss
+                    batchvar = 'batch' + param
+                    batchstats['%s_mean' % param] = (((ss - 1)*batchstats['%s_mean_prv' % param]) + eval(batchvar))/ss
+                    batchstats['%s_variance' % param] = (((ss - 1)*batchstats['%s_variance' % param]) + (eval(batchvar) - batchstats['%s_mean_prv' % param])*(eval(batchvar) - batchstats['%s_mean' % param]))/ss
                     batchstats['%s_sd' % param] = batchstats['%s_variance' % param]**0.5
                     batchstats['%s_mean_prv' % param] = batchstats['%s_mean' % param]
 
@@ -2928,16 +2924,12 @@ class ProgressView(RowContainer):
             yield r
         end = time.time()
         elapsedtime = end - start
-
-        print(batchstats['rate_mean'])
-        print(batchstats['time_mean'])
-
         try:
             rate = int(n / elapsedtime)
         except ZeroDivisionError:
             rate = 0
-        v = (n, elapsedtime, rate)
-        message = self.prefix + '%s rows in %.2fs (%s row/s)' % v
+        v = (n, elapsedtime, rate, batchstats['time_mean'], batchstats['time_sd'], batchstats['rate_mean'], batchstats['rate_sd'])
+        message = self.prefix + '%s rows in %.2fs (%s row/s); batch mean: %.2fs, sd: %.2fs (mean: %s row/s, sd: %s row/s)' % v
         print(message, file=self.out)
         if hasattr(self.out, 'flush'):
             self.out.flush()
