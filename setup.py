5c8cb0d8d6bd773373be66f352808343425822b2
Alistair Miles <alimanfoo@googlemail.com>
2014-12-23 16:05:54 +0000

    remove six as not needed; update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,9 @@ setup(
     long_description=open('README.txt').read(),
     classifiers=['Intended Audience :: Developers',
                  'License :: OSI Approved :: MIT License',
-                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.4',
                  'Topic :: Software Development :: Libraries :: Python Modules'
                  ]
 )
