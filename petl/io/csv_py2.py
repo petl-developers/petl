7bd1719cec8f4f77fc4ce651df77dd94cf6872dc
Viliam Segeda <viliam.segeda@gmail.com>
2016-05-31 18:04:22 +0200

    resolves #377

--- petl/io/csv_py2.py
+++ petl/io/csv_py2.py
@@ -147,7 +147,8 @@ class UnicodeReader:
 
     def next(self):
         row = self.reader.next()
-        return [unicode(s, 'utf-8') for s in row]
+        return [unicode(s, 'utf-8') if isinstance(s, basestring) else s 
+                    for s in row]
 
     def __iter__(self):
         return self
