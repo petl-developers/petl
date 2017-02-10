9f396d63799ed1edc81d6dcd14977b64841fc475
Brad Maggard <bradmaggard@MB-H26815-2.local>
2017-02-10 16:13:43 -0500

    fix #412: distinct() filters None

--- petl/transform/dedup.py
+++ petl/transform/dedup.py
@@ -416,13 +416,14 @@ class DistinctView(Table):
         # the field selection
         getkey = operator.itemgetter(*indices)
 
+        INIT = object()
         if self.count:
             hdr = tuple(hdr) + (self.count,)
             yield hdr
-            previous = None
+            previous = INIT
             n_dup = 1
             for row in it:
-                if previous is None:
+                if previous is INIT:
                     previous = row
                 else:
                     kprev = getkey(previous)
@@ -437,7 +438,7 @@ class DistinctView(Table):
             yield tuple(previous) + (n_dup,)
         else:
             yield tuple(hdr)
-            previous_keys = None
+            previous_keys = INIT
             for row in it:
                 keys = getkey(row)
                 if keys != previous_keys:
