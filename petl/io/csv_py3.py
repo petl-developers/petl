f6430008d5c522ee7ebc077baab3e5016343ed26
robinmoss <robinmoss@e2eservices.co.uk>
2017-04-06 11:33:52 +0100

    Added support for headerless csv files
    Added an optional header argument from fromcsv/fromtsv to allow
    specificying headers for the file instead of taking the first row

--- petl/io/csv_py3.py
+++ petl/io/csv_py3.py
@@ -19,13 +19,16 @@ def fromcsv_impl(source, **kwargs):
 
 class CSVView(Table):
 
-    def __init__(self, source, encoding, errors, **csvargs):
+    def __init__(self, source, encoding, errors, header, **csvargs):
             self.source = source
             self.encoding = encoding
             self.errors = errors
             self.csvargs = csvargs
+            self.header = header
 
     def __iter__(self):
+        if self.header is not None:
+          yield tuple(self.header)
         with self.source.open('rb') as buf:
             csvfile = io.TextIOWrapper(buf, encoding=self.encoding,
                                        errors=self.errors, newline='')
