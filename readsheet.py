9cda737f8a25ab9632a67a4d218b38866ba87adc
Manuel Belmadani <manuel.belmadani@gmail.com>
2018-03-09 14:17:48 -0800

    Printing version numbers.

--- readsheet.py
+++ readsheet.py
@@ -1,4 +1,8 @@
 import petl
+import openpyxl
+
+print "petl version", petl.__version__
+print "openpyxl version", openpyxl.__version__
 
 # Default settings
 data = petl.io.xlsx.fromxlsx("100krow.xlsx")
@@ -6,4 +10,4 @@ print "Number of lines with default settings:", len(data)
 
 # Read only set to false
 data = petl.io.xlsx.fromxlsx("100krow.xlsx", read_only=False)
-print "Number of lines with default settings:", len(data)
+print "Number of lines with `read_only=False` settings:", len(data)
