8d3b892330c6d627998ae0f1c46f44301fbd92c3
gunq <guilherme.junqueira@gove.digital>
2019-05-28 16:55:35 -0300

    Passing along **kwargs to xlrd and encoding to openpyxl

--- petl/io/xlsx.py
+++ petl/io/xlsx.py
@@ -86,7 +86,7 @@ def toxlsx(tbl, filename, sheet=None, encoding=None):
     import openpyxl
     if encoding is None:
         encoding = locale.getpreferredencoding()
-    wb = openpyxl.Workbook(write_only=True)
+    wb = openpyxl.Workbook(encoding=encoding, write_only=True)
     ws = wb.create_sheet(title=sheet)
     for row in tbl:
         ws.append(row)
