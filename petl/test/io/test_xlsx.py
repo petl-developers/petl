6e21fc0db9e67114d434438e3cc4daec3a673cf1
Juarez Rudsatz <juarez.rudsatz@ceabs.net>
2020-08-07 20:12:32 -0300

    Allow toxlsx() to overwrite add or replace a worksheet

--- petl/test/io/test_xlsx.py
+++ petl/test/io/test_xlsx.py
@@ -3,14 +3,14 @@ from __future__ import absolute_import, print_function, division
 
 
 import sys
-import pkg_resources
 from datetime import datetime
 from tempfile import NamedTemporaryFile
 
+import pkg_resources
 
 import petl as etl
 from petl.io.xlsx import fromxlsx, toxlsx, appendxlsx
-from petl.test.helpers import ieq
+from petl.test.helpers import ieq, eq_
 
 
 try:
@@ -79,7 +79,7 @@ else:
                ('B', 2),
                ('C', 2),
                (u'é', datetime(2012, 1, 1)))
-        f = NamedTemporaryFile(delete=False, suffix='.xlsx')
+        f = NamedTemporaryFile(delete=True, suffix='.xlsx')
         f.close()
 
         # test toxlsx
@@ -98,7 +98,7 @@ else:
                ('B', 2),
                ('C', 2),
                (u'é', datetime(2012, 1, 1)))
-        f = NamedTemporaryFile(delete=False, suffix='.xlsx')
+        f = NamedTemporaryFile(delete=True, suffix='.xlsx')
         f.close()
         toxlsx(tbl, f.name)
         actual = fromxlsx(f.name)
@@ -110,7 +110,7 @@ else:
                ('B', 2),
                ('C', 2),
                (u'é', datetime(2012, 1, 1)))
-        f = NamedTemporaryFile(delete=False, suffix='.xlsx')
+        f = NamedTemporaryFile(delete=True, suffix='.xlsx')
         f.close()
         tbl = etl.wrap(tbl)
         tbl.toxlsx(f.name, 'Sheet1')
@@ -120,3 +120,100 @@ else:
         expect = tbl.cat(tbl)
         ieq(expect, actual)
 
+    def test_toxlsx_overwrite():
+        tbl = (('foo', 'bar'),
+               ('A', 1),
+               ('B', 2),
+               ('C', 2),
+               (u'é', datetime(2012, 1, 1)))
+        f = NamedTemporaryFile(delete=False, suffix='.xlsx')
+        f.close()
+
+        toxlsx(tbl, f.name, 'Sheet1', mode="overwrite")
+        wb = openpyxl.load_workbook(f.name, read_only=True)
+        eq_(1, len(wb.sheetnames))
+
+    def test_toxlsx_replace_file():
+        tbl = (('foo', 'bar'),
+               ('A', 1),
+               ('B', 2),
+               ('C', 2),
+               (u'é', datetime(2012, 1, 1)))
+        f = NamedTemporaryFile(delete=True, suffix='.xlsx')
+        f.close()
+
+        toxlsx(tbl, f.name, 'Sheet1', mode="overwrite")
+        toxlsx(tbl, f.name, sheet=None, mode="replace")
+        wb = openpyxl.load_workbook(f.name, read_only=True)
+        eq_(1, len(wb.sheetnames))
+
+    def test_toxlsx_replace_sheet():
+        tbl = (('foo', 'bar'),
+               ('A', 1),
+               ('B', 2),
+               ('C', 2),
+               (u'é', datetime(2012, 1, 1)))
+        f = NamedTemporaryFile(delete=True, suffix='.xlsx')
+        f.close()
+
+        toxlsx(tbl, f.name, 'Sheet1', mode="overwrite")
+        toxlsx(tbl, f.name, 'Sheet1', mode="replace")
+        wb = openpyxl.load_workbook(f.name, read_only=True)
+        eq_(1, len(wb.sheetnames))
+
+    def test_toxlsx_replace_sheet_nofile():
+        tbl = (('foo', 'bar'),
+               ('A', 1),
+               ('B', 2),
+               ('C', 2),
+               (u'é', datetime(2012, 1, 1)))
+        f = NamedTemporaryFile(delete=True, suffix='.xlsx')
+        f.close()
+
+        toxlsx(tbl, f.name, 'Sheet1', mode="replace")
+        wb = openpyxl.load_workbook(f.name, read_only=True)
+        eq_(1, len(wb.sheetnames))
+
+    def test_toxlsx_add_nosheet():
+        tbl = (('foo', 'bar'),
+               ('A', 1),
+               ('B', 2),
+               ('C', 2),
+               (u'é', datetime(2012, 1, 1)))
+        f = NamedTemporaryFile(delete=True, suffix='.xlsx')
+        f.close()
+
+        toxlsx(tbl, f.name, 'Sheet1', mode="overwrite")
+        toxlsx(tbl, f.name, None, mode="add")
+        wb = openpyxl.load_workbook(f.name, read_only=True)
+        eq_(2, len(wb.sheetnames))
+
+    def test_toxlsx_add_sheet_nomatch():
+        tbl = (('foo', 'bar'),
+               ('A', 1),
+               ('B', 2),
+               ('C', 2),
+               (u'é', datetime(2012, 1, 1)))
+        f = NamedTemporaryFile(delete=True, suffix='.xlsx')
+        f.close()
+
+        toxlsx(tbl, f.name, 'Sheet1', mode="overwrite")
+        toxlsx(tbl, f.name, 'Sheet2', mode="add")
+        wb = openpyxl.load_workbook(f.name, read_only=True)
+        eq_(2, len(wb.sheetnames))
+
+    def test_toxlsx_add_sheet_match():
+        tbl = (('foo', 'bar'),
+               ('A', 1),
+               ('B', 2),
+               ('C', 2),
+               (u'é', datetime(2012, 1, 1)))
+        f = NamedTemporaryFile(delete=True, suffix='.xlsx')
+        f.close()
+
+        toxlsx(tbl, f.name, 'Sheet1', mode="overwrite")
+        try:
+            toxlsx(tbl, f.name, 'Sheet1', mode="add")
+            assert False, 'Adding duplicate sheet name did not fail'
+        except ValueError:
+            pass
