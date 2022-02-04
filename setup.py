0373331c007eb11a1d50a68331c7b88e616725b1
Juarez Rudsatz <juarez.rudsatz.ter@estacio.br>
2022-02-03 21:05:38 -0300

    pip: annouce python 3.9

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3.7',
                  'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
                  'Topic :: Software Development :: Libraries :: Python Modules'
                  ]
 )
