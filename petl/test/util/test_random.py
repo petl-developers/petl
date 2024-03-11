41a62b9894cc1cf4b83983a9204964e3bfe31376
Wil Thieme <wil.thieme@protonmail.com>
2024-03-11 14:23:25 -0400

    Fix: DeprecationWarning: Seeding based on hashing is deprecated since Python 3.9 and will be removed in a subsequent version.

--- petl/test/util/test_random.py
+++ petl/test/util/test_random.py
@@ -1,7 +1,21 @@
 import random
+import time
 from functools import partial
 
-from petl.util.random import randomtable, RandomTable, dummytable, DummyTable
+from petl.util.random import randomseed, randomtable, RandomTable, dummytable, DummyTable
+
+
+def test_randomseed():
+    """
+    Ensure that randomseed provides a non-empty string that changes.
+    """
+    seed_1 = randomseed()
+    time.sleep(1)
+    seed_2 = randomseed()
+
+    assert isinstance(seed_1, str)
+    assert seed_1 != ""
+    assert seed_1 != seed_2
 
 
 def test_randomtable():
