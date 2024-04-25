60818d522e252ce35644d1222077f24b36eef494
Yaniv Aknin <yaniv@aknin.name>
2024-04-25 06:24:37 +0100

    Fix fromjson() to support reading from stdin

--- petl/test/test_executable.py
+++ petl/test/test_executable.py
@@ -3,8 +3,20 @@ from __future__ import print_function, division, absolute_import
 import subprocess
 
 def test_executable():
-    result = subprocess.run("""
+    result = subprocess.check_output("""
         (echo foo,bar ; echo a,b; echo c,d) |
         petl 'fromcsv().cut("foo").head(1).tocsv()'
-    """, shell=True, check=True, capture_output=True)
-    assert result.stdout == b'foo\r\na\r\n'
+    """, shell=True)
+    assert result == b'foo\r\na\r\n'
+
+def test_json_stdin():
+    result = subprocess.check_output("""
+        echo '[{"foo": "a", "bar": "b"}]' |
+        petl 'fromjson().tocsv()'
+    """, shell=True)
+    assert result == b'foo,bar\r\na,b\r\n'
+    result = subprocess.check_output("""
+        ( echo '{"foo": "a", "bar": "b"}' ; echo '{"foo": "c", "bar": "d"}' ) |
+        petl 'fromjson(lines=True).tocsv()'
+    """, shell=True)
+    assert result == b'foo,bar\r\na,b\r\nc,d\r\n'
