2d3df33c4d767219accd22c02213503b3243aacb
Juarez Rudsatz <juarezr@gmail.com>
2025-07-10 15:08:00 -0300

    fix: make local pytest work by making optional the import of sqlalchemy

--- petl/test/io/test_json_column.py
+++
@@ -1,8 +0,0 @@
-from petl.io.db_create import make_sqlalchemy_column
-from sqlalchemy import JSON
-
-def test_json_inference():
-    data = [{'a': 1}, {'b': 2}, None]
-    col = make_sqlalchemy_column(data, 'payload')
-    assert col.name == 'payload'
-    assert isinstance(col.type, JSON)
